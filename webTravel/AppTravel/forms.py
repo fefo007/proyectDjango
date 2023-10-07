6ae02dd4ba2d8684080a66b47c3c31434a73d98d
Federico Fracaro <federicohfracaro@gmail.com>
2023-10-07 18:58:25 -0300

    final-terminado

--- webTravel/AppTravel/forms.py
+++ webTravel/AppTravel/forms.py
@@ -52,19 +52,19 @@ class UserEditForm(UserChangeForm):
 
     password = forms.CharField(help_text='',widget=forms.HiddenInput,required=False,label='')
 
-    password1 = forms.CharField(label='password')
-    password2 = forms.CharField(label='repetir password')
+    # password1 = forms.CharField(label='password')
+    # password2 = forms.CharField(label='repetir password')
 
     class Meta:
         model = User
         fields = ['username','first_name','last_name','email']
 
-    def clean_password(self):
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
-        if password1 != password2:
-            raise forms.ValidationError('las contraseñas no coinciden')
-        return password2
+    # def clean_password(self):
+    #     password1 = self.cleaned_data['password1']
+    #     password2 = self.cleaned_data['password2']
+    #     if password1 != password2:
+    #         raise forms.ValidationError('las contraseñas no coinciden')
+    #     return password2
     
 
 class AvatarEditForm(forms.ModelForm):
